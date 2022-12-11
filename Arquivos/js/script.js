0058aae9acbbc40c5caccb5cc770279e886ea169
joaopedrobenassi <joaopedrobenassi@gmail.com>
2022-12-11 16:06:02 -0300

    Iniciando logica dos eventos em JS

--- Arquivos/js/script.js
+++ Arquivos/js/script.js
@@ -1,5 +1,20 @@
 // Obtendo os elementos
-const inpuUsuario = document.querySelector('.campoInput input')
+const inputUsuario = document.querySelector('.campoInput input')
 const botaoAdd = document.querySelector('.campoInput button')
 const listaDeTarefas = document.querySelector('.listaDeTarefas')
 const botaoLimpar = document.querySelector('footer button')
+
+//Evento onkeyup
+inputUsuario.onkeyup = () => { //onkeyup toda vez que a tecla parar de ser pressionada executará função
+    let valorInserido = inputUsuario.value //Obtendo o valor inserido pelo usuario
+    if(valorInserido.trim() != 0){// Se o valor do usuario não for apenas espaços
+        botaoAdd.classList.add("active") //Ative o botão adicionar
+    }else {
+        botaoAdd.classList.remove("active") // Desativa o botão
+    }
+}
+
+botaoAdd.onlick = () => { //Quano o usuário apertar o botão irá ocorres essa função
+    let valorInserido = inputUsuario.value // Pega o valor digitado pelo usuário
+    
+}
