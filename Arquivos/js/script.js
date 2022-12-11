f294c146ef9cbb44807996c8f69bbe8e40d49172
joaopedrobenassi <joaopedrobenassi@gmail.com>
2022-12-11 15:03:33 -0300

    Selecioanando elementos DOM

---
+++ Arquivos/js/script.js
@@ -0,0 +1,5 @@
+// Obtendo os elementos
+const inpuUsuario = document.querySelector('.campoInput input')
+const botaoAdd = document.querySelector('.campoInput button')
+const listaDeTarefas = document.querySelector('.listaDeTarefas')
+const botaoLimpar = document.querySelector('footer button')
