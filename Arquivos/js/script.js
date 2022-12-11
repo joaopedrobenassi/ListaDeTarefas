3b50649bbb40ff68e3a778040c67deb6b9af333a
joaopedrobenassi <joaopedrobenassi@gmail.com>
2022-12-11 18:26:13 -0300

    Desenvolvendo logica dos eventos

--- Arquivos/js/script.js
+++ Arquivos/js/script.js
@@ -8,13 +8,26 @@ const botaoLimpar = document.querySelector('footer button')
 inputUsuario.onkeyup = () => { //onkeyup toda vez que a tecla parar de ser pressionada executará função
     let valorInserido = inputUsuario.value //Obtendo o valor inserido pelo usuario
     if(valorInserido.trim() != 0){// Se o valor do usuario não for apenas espaços
-        botaoAdd.classList.add("active") //Ative o botão adicionar
+        botaoAdd.classList.add('active') //Ative o botão adicionar
     }else {
-        botaoAdd.classList.remove("active") // Desativa o botão
+        botaoAdd.classList.remove('active') // Desativa o botão
     }
 }
+mostrarTarefas()
 
 botaoAdd.onlick = () => { //Quano o usuário apertar o botão irá ocorres essa função
     let valorInserido = inputUsuario.value // Pega o valor digitado pelo usuário
+    let getLocalStorageData = localStorage.getItem('Nova Tarefa') // Vai obter o valor do armazenamento que tiver a chave Nova Tarefa
+    if(getLocalStorageData == null){ // Se o localStorage não tiver dados
+        listArray = [] //Criando um array vazio
+    }else {
+        listArray = JSON.parse(getLocalStorageData) //Transformando string em objeto
+    }
+    listArray.push(valorInserido) //adicionando valor inserido ao array
+    localStorage.setItem('Nova Tarefa', JSON.stringify(listArray)) //Transforma o objeto JSON em string
+    botaoAdd.classList.remove('active') //Desativa o botao apos a nova tarefa ser adicionada
+
+}
+function mostrarTarefas() {
     
 }
